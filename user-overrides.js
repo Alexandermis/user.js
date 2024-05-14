1ab02e046444df0b8036dde480bd70864771332d
Alexander <alexandermis@web.de>
2024-05-14 09:08:21 +0200

    DOT restore session but I need the clear the histroy  DuckduckGo search EN lan

---
+++ user-overrides.js
@@ -0,0 +1,48 @@
+//DOT
+user_pref("network.trr.uri", "https://dns.quad9.net/dns-query");
+user_pref("network.trr.mode", 2);
+//restore session
+user_pref("browser.startup.page", 3);
+user_pref("browser.startup.pagfge", 3);
+
+user_pref("browser.sessionstore.max_tabs_undo", 20);
+user_pref("browser.sessionstore.resume_session_once", true);
+
+user_pref("privacy.clearOnShutdown.history", false);
+user_pref("privacy.clearOnShutdown.sessions", false); 			
+user_pref("privacy.clearOnShutdown.siteSettings", false); 
+user_pref("browser.sessionstore.max_tabs_undo", 50);			
+user_pref("browser.sessionstore.max_windows_undo", 10);			
+
+	
+
+user_pref("extensions.pocket.enabled", false); 		
+
+user_pref("privacy.clearOnShutdown.cookies", true);	
+
+user_pref("browser.search.defaultenginename", "DuckDuckGo");
+
+user_pref("privacy.clearOnShutdown.cache", true);		
+user_pref("browser.sessionstore.restore_on_demand", true);		
+user_pref("browser.sessionstore.privacy_level", 0);		
+user_pref("browser.sessionstore.resume_session_once", true);		
+
+
+//Region
+user_pref("browser.search.region", "DE");
+//Firefox lan.
+user_pref("intl.locale.requested", "en-US,de");
+
+user_pref("extensions.update.enabled", true); //0303
+user_pref("extensions.update.autoUpdateDefault", true); //0304
+
+
+
+//Test
+//user_pref("privacy.cpd.cookies", false); //2804 
+user_pref("webgl.disabled", true);
+user_pref("media.eme.enabled", true);
+//user_pref("browser.search.suggest.enabled", false);
+//user_pref("browser.urlbar.suggest.searches", false);///
+
+user_pref("browser.sessionstore.max_windows_undo", 3);
